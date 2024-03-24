2ec1d26e15e9ecb0c6d448486ef614327d18399c
ZeyaKim <chlwltjrdev@gmail.com>
2024-03-24 12:57:44 +0900

    Add URL patterns for post list and post detail views

--- blog/urls.py
+++ blog/urls.py
@@ -1,1 +1,7 @@
-urlpatterns = []
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("posts/", views.PostList.as_view(), name="post_list"),
+    path("posts/<int:pk>/", views.PostDetail.as_view(), name="post_detail"),
+]
